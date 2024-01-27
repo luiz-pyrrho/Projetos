d59d73b32742eec6e74d86981e308c5e58313139
Luiz Pyrrho <140938366+luiz-pyrrho@users.noreply.github.com>
2024-01-27 01:32:28 -0300

    Add files via upload

---
+++ Argumento2.java
@@ -0,0 +1,9 @@
+package curso1;
+
+public class Argumento2 {
+	public static void main(String[] args) {
+		String[] argumento = {" tudo bem ?"};
+		args = argumento;
+		System.out.println("Olá, mundo" + args[0]);
+	}
+}
---
+++ Argumentos.java
@@ -0,0 +1,12 @@
+package curso1;
+
+public class Argumentos {
+    public static void main(String[] args) {
+       
+    	String[]meusArgs = {"2"};
+       
+        args= meusArgs;
+
+        System.out.println("Você digitou " + args[0]);
+    }
+}
